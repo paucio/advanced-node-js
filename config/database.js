243c64bf414d4a0fc0421399dedd8431bef2ac54
skylock <emanuil.copil@gmail.com>
2020-12-15 15:21:28 +0200

    refactoring db config

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const db = {
+  host: process.env.MONGO_HOST || 'localhost',
+  port: process.env.MONGO_PORT || 27017
+};
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 // Keep trying to send operations for 5 seconds
+};
+
+const init = async () => {
+  /* MONGOOSE SETUP */
+  const connectionString = `mongodb://${db.host}:${db.port}/blog_everyone:`;
+
+  await mongoose.connect(connectionString, options).catch(() => {
+    console.log(`Error connecting to mongo ${connectionString}`);
+  });
+};
+
+
+module.exports = {
+  init
+};
