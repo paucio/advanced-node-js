cc4ebd11262b65309c581b9eebd39840514b21b2
Tudor Butiri <tudor.butiri@3pillarglobal.com>
2021-02-08 14:14:10 +0200

    partial

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '292193279240-bs7v8rd2qsl761v84i9dicp7l3r8tj2a.apps.googleusercontent.com',
-  googleClientSecret: 'BtmEgsfLP8Q5Hhlksq0DWIEb',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
-};
